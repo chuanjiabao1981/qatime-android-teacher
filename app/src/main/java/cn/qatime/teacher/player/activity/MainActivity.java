21a590c0c54db4dc531e48fd9db4678f77805694
lungtify <15617685965@163.com>
2016-10-24 14:05:47 +0800

    初始化项目

---
+++ app/src/main/java/cn/qatime/teacher/player/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package cn.qatime.teacher.player.activity;
+
+import android.os.Bundle;
+
+import cn.qatime.teacher.player.R;
+import cn.qatime.teacher.player.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public int getContentView() {
+        return R.layout.activity_main;
+    }
+}
---
+++ app/src/main/java/cn/qatime/teacher/player/base/BaseActivity.java
@@ -0,0 +1,135 @@
+package cn.qatime.teacher.player.base;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+
+import cn.qatime.teacher.player.activity.MainActivity;
+import cn.qatime.teacher.player.R;
+import libraryextra.utils.StringUtils;
+
+/**
+ * 基础类
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    private RequestQueue Queue;
+    private AlertDialog alertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        PushAgent.getInstance(this).onAppStart();
+        if (getContentView() != 0) {
+            setContentView(getContentView());
+        }
+        Queue = BaseApplication.getRequestQueue();
+    }
+
+    /**
+     * 布局
+     *
+     * @return 布局id
+     */
+    public abstract int getContentView();
+
+    public void setTitle(String text) {
+        if (StringUtils.isNullOrBlanK(text)) {
+            throw new IllegalStateException("text can not be a null object");
+        }
+        if (findViewById(R.id.title) != null) {
+            ((TextView) findViewById(R.id.title)).setText(text);
+        }
+    }
+
+    public void setRightImage(int resource, View.OnClickListener listener) {
+        if (findViewById(R.id.right) != null) {
+            ((ImageView) findViewById(R.id.right)).setImageResource(resource);
+            findViewById(R.id.right).setOnClickListener(listener);
+        }
+
+    }
+
+    public void setRightText(String text, View.OnClickListener listener) {
+        if (StringUtils.isNullOrBlanK(text)) {
+            throw new IllegalArgumentException("text can not be a null object");
+        }
+        if (findViewById(R.id.right_text) != null) {
+            ((TextView) findViewById(R.id.right_text)).setText(text);
+            findViewById(R.id.right_text).setOnClickListener(listener);
+        }
+
+    }
+
+    public void backClick(View v) {
+        this.finish();
+    }
+
+
+    /**
+     * 设备已在其他地方登陆
+     */
+    public void tokenOut() {
+        BaseApplication.clearToken();
+        if (alertDialog == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            alertDialog = builder.create();
+            View view = View.inflate(this, R.layout.dialog_confirm, null);
+            TextView text = (TextView) view.findViewById(R.id.text);
+            text.setText(getResourceString(R.string.login_has_expired));
+            Button confirm = (Button) view.findViewById(R.id.confirm);
+            confirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alertDialog.dismiss();
+                    out();
+                }
+            });
+            alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    out();
+                }
+            });
+            alertDialog.show();
+            alertDialog.setContentView(view);
+//            WindowManager.LayoutParams attributes = alertDialog.getWindow().getAttributes();
+//            attributes.width= ScreenUtils.getScreenWidth(getApplicationContext())- DensityUtils.dp2px(getApplicationContext(),20)*2;
+//            alertDialog.getWindow().setAttributes(attributes);
+        }
+        if (!alertDialog.isShowing()) {
+            alertDialog.show();
+        }
+    }
+
+    private void out() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("out", "out");
+        startActivity(intent);
+//        this.finish();
+    }
+
+    public <T> Request<T> addToRequestQueue(Request<T> request) {
+        return Queue.add(request);
+    }
+
+    public void cancelAll(final Object tag) {
+        Queue.cancelAll(tag);
+    }
+
+    public void cancelAll(final RequestQueue.RequestFilter filter) {
+        Queue.cancelAll(filter);
+    }
+
+    protected String getResourceString(int id) {
+        return getResources().getString(id);
+    }
+}
