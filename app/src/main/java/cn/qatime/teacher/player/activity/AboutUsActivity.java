c8a6f821c6566ce75229c2eef2890cfb20819d37
TianhaoranLY <tianhaoran@bjwwtd.com>
2016-10-27 11:39:46 +0800

    QTA-68 设置中心初步实现

--- app/src/main/java/cn/qatime/teacher/player/activity/AboutUsActivity.java
+++ app/src/main/java/cn/qatime/teacher/player/activity/AboutUsActivity.java
@@ -1,13 +1,17 @@
 package cn.qatime.teacher.player.activity;
 
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import cn.qatime.teacher.player.R;
 import cn.qatime.teacher.player.base.BaseActivity;
@@ -59,6 +63,10 @@ public class AboutUsActivity extends BaseActivity {
                     public void onClick(View v) {
                         alertDialog.dismiss();
                         Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone.getText()));
+                        if (ActivityCompat.checkSelfPermission(AboutUsActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(AboutUsActivity.this, "权限被拒绝", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
                         startActivity(intent);
                     }
                 });
