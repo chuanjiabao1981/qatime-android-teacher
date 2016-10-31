8f0a5563d12110a18a35d2acc1b6cb1d3a30437c
TianhaoranLY <tianhaoran@bjwwtd.com>
2016-10-31 12:43:59 +0800

    QTA-68 匹配密码

--- app/src/main/java/cn/qatime/teacher/player/activity/ChangePasswordActivity.java
+++ app/src/main/java/cn/qatime/teacher/player/activity/ChangePasswordActivity.java
@@ -3,10 +3,11 @@ package cn.qatime.teacher.player.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,8 +39,8 @@ public class ChangePasswordActivity extends BaseActivity implements View.OnClick
     private EditText password;
     private EditText newPassword;
     private EditText confirmNewPassword;
-    private ImageView matchPwd1;
-    private ImageView matchPwd2;
+    private TextView matchPwd1;
+    private TextView matchPwd2;
     private Button buttonOver;
     private String password1;
     private String password2;
@@ -52,6 +53,8 @@ public class ChangePasswordActivity extends BaseActivity implements View.OnClick
         newPassword = (EditText) findViewById(R.id.new_password);
         confirmNewPassword = (EditText) findViewById(R.id.confirm_new_password);
         buttonOver = (Button) findViewById(R.id.button_over);
+        matchPwd1 = (TextView) findViewById(R.id.match_pwd1);
+        matchPwd2 = (TextView) findViewById(R.id.match_pwd2);
     }
 
     @Override
@@ -73,7 +76,41 @@ public class ChangePasswordActivity extends BaseActivity implements View.OnClick
         password.setHint(StringUtils.getSpannedString(this, R.string.hint_input_current_password));
         newPassword.setHint(StringUtils.getSpannedString(this, R.string.hint_input_new_password));
         confirmNewPassword.setHint(StringUtils.getSpannedString(this, R.string.hint_confirm_new_password));
+        newPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                password2 = newPassword.getText().toString().trim();
+                matchPwd2.setVisibility(View.INVISIBLE);
+                matchPwd1.setVisibility(StringUtils.isGoodPWD(password2) ? View.VISIBLE:View.INVISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        confirmNewPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                password3 = confirmNewPassword.getText().toString().trim();
+                matchPwd2.setVisibility(password3.equals(password2) ? View.VISIBLE:View.INVISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         forgetPassword.setOnClickListener(this);
         buttonOver.setOnClickListener(this);
@@ -82,8 +119,8 @@ public class ChangePasswordActivity extends BaseActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         password1 = password.getText().toString().trim();
-        password2 = newPassword.getText().toString().trim();
-        password3 = confirmNewPassword.getText().toString().trim();
+//        password2 = newPassword.getText().toString().trim();
+//        password3 = confirmNewPassword.getText().toString().trim();
         switch (v.getId()) {
             case R.id.forget_password:
                 Intent intent = new Intent(this, ForgetPasswordActivity.class);
